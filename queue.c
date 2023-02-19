59e1fa005a56aa5e798f85231730a00e45afdede
Ahsen <ahsenlaboratory@gmail.com>
2023-02-19 16:22:55 +0800

    Add a function to delete the mid element of queue
    Add a 'q_delete_mid' function.

--- queue.c
+++ queue.c
@@ -110,6 +110,19 @@ int q_size(struct list_head *head)
 bool q_delete_mid(struct list_head *head)
 {
     // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return false;
+
+    struct list_head *mid = head->next, *fast = head->next;
+    while (fast != head && fast->next != head) {
+        mid = mid->next;
+        fast = fast->next->next;
+    }
+
+    element_t *entry = list_entry(mid, element_t, list);
+    list_del_init(mid);
+    free(entry->value);
+    free(entry);
     return true;
 }
 
